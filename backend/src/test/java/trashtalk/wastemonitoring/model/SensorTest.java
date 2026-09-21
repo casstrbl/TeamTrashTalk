4eff0aa0bfb82df92b793798b0cc98844a8648d1
brookeleeann <135168205+brookeleeann@users.noreply.github.com>
2026-09-20 21:35:08 -0700

    Replace test files with JUnit test files

---
+++ backend/src/test/java/trashtalk/wastemonitoring/model/SensorTest.java
@@ -0,0 +1,14 @@
+package test.java.trashtalk.wastemonitoring.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SensorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
