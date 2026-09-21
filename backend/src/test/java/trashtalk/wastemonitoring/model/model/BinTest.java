7d4c6c4a5dc3232569833c903a3e5b763a623aba
brookeleeann <135168205+brookeleeann@users.noreply.github.com>
2026-09-20 21:30:38 -0700

    Replaced test files with JUnit test files

---
+++ backend/src/test/java/trashtalk/wastemonitoring/model/model/BinTest.java
@@ -0,0 +1,14 @@
+package test.java.trashtalk.wastemonitoring.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BinTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
