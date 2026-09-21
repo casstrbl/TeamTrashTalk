d82877663ce40357a61d537c8392303d788ebbf2
brookeleeann <135168205+brookeleeann@users.noreply.github.com>
2026-09-20 21:37:15 -0700

    Replace old service test files with JUnit files

---
+++ backend/src/test/java/trashtalk/wastemonitoring/service/BinServiceTest.java
@@ -0,0 +1,14 @@
+package test.java.trashtalk.wastemonitoring.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BinServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
