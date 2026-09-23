b6fe1a268b5d1c30371160086b46d5cb117ff13d
AudreyDike-csus <audreydike@csus.edu>
2026-09-23 07:35:01 -0700

    Add tests for Bin model
    Added unit tests for Bin class functionality including initial waste height, bin height constant, available bin types, and statuses.

--- backend/src/test/java/trashtalk/wastemonitoring/model/BinTest.java
+++ backend/src/test/java/trashtalk/wastemonitoring/model/BinTest.java
@@ -4,11 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.java.trashtalk.wastemonitoring.model.Bin;
+
 class BinTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void newBinStartsEmpty() {
+        Bin bin = new Bin(1, "Library", Bin.BinType.LANDFILL);
 
+        assertEquals(0.0, bin.getWasteHeight());
+    }
+
+    @Test
+    void binHeightIs77Centimeters() {
+        assertEquals(77, Bin.BIN_HEIGHT);
+    }
+
+    @Test
+    void binTypesAreAvailable() {
+        assertEquals(3, Bin.BinType.values().length);
+        assertNotNull(Bin.BinType.LANDFILL);
+        assertNotNull(Bin.BinType.RECYCLING);
+        assertNotNull(Bin.BinType.COMPOST);
+    }
+
+    @Test
+    void binStatusesAreAvailable() {
+        assertEquals(3, Bin.BinStatus.values().length);
+        assertNotNull(Bin.BinStatus.GREEN);
+        assertNotNull(Bin.BinStatus.YELLOW);
+        assertNotNull(Bin.BinStatus.RED);
+    }
 }
