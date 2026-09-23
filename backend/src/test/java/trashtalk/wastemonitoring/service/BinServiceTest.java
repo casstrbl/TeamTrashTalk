733dfb1ae50776c1099958d0bc9720aa25bb96f9
AudreyDike-csus <audreydike@csus.edu>
2026-09-23 06:39:51 -0700

    Add BinService fullness and status tests
    Added unit tests for BinService methods to check new bin percentage and status.

--- backend/src/test/java/trashtalk/wastemonitoring/service/BinServiceTest.java
+++ backend/src/test/java/trashtalk/wastemonitoring/service/BinServiceTest.java
@@ -4,11 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.java.trashtalk.wastemonitoring.model.Bin;
+import main.java.trashtalk.wastemonitoring.service.BinService;
+
 class BinServiceTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void testNewBinPercentageIsZero() {
+        Bin bin = new Bin(1, "Library", Bin.BinType.LANDFILL);
+        BinService binService = new BinService();
 
+        double percentage = binService.getPercentage(bin);
+
+        assertEquals(0.0, percentage);
+    }
+
+    @Test
+    void testNewBinStatusIsGreen() {
+        Bin bin = new Bin(1, "Library", Bin.BinType.LANDFILL);
+        BinService binService = new BinService();
+
+        Bin.BinStatus status = binService.getStatus(bin);
+
+        assertEquals(Bin.BinStatus.GREEN, status);
+    }
 }
